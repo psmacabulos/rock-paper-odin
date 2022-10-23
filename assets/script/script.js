7bf23882d9d8e4b4a1f0b434cd5be54186706607
psmacabulos <patrick.macabulos@gmail.com>
2022-10-23 21:35:32 +0800

    Improved script and added more styles

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
 // define the constant first
 const CHOICES = ['Rock', 'Paper', 'Scissors'];
+let gameover = false;
 let playerScore = 0;
 let computerScore = 0;
 
@@ -7,9 +8,13 @@ let player = document.querySelector('.player_score');
 let computer = document.querySelector('.computer_score');
 
 const reset = () => {
+  gameover = false;
+  playAgainBtn.style = 'opacity: 0';
   playerScore = 0;
+  player.innerText = playerScore;
   computerScore = 0;
-  info.innerHTML = '';
+  computer.innerText = computerScore;
+  info.innerHTML = 'Choose Your Weapon!';
 };
 
 function getComputerChoice() {
@@ -42,31 +47,41 @@ const info = document.querySelector('.info');
 
 const checkGame = (player, computer) => {
   let result = false;
-
+  if (player == 5 || computer == 5) {
+    gameover = true;
+    result = true;
+    playAgainBtn.style = 'opacity:1';
+  }
   if (player == 5) {
     let p = document.createElement('p');
     p.innerText = `Gameover. You won the game!`;
     p.style.cssText = 'color:red;';
     info.appendChild(p);
-    result = true;
   } else if (computer == 5) {
     let p = document.createElement('p');
     p.innerText = `Gameover. Computer won the game!`;
     p.style.cssText = 'color:red;';
     info.appendChild(p);
-    result = true;
   }
 
   return result;
 };
 
+let playAgainBtn = document.querySelector('#play_again');
+
+playAgainBtn.addEventListener('click', () => {
+  reset();
+});
+
 buttons.forEach((btn) => {
   let playerSel = btn.getAttribute('id');
   btn.addEventListener('click', () => {
-    let result = playRound(playerSel, getComputerChoice());
-    let para1 = document.createElement('p');
-    para1.innerText = result;
-    info.innerHTML = result;
-    checkGame(playerScore, computerScore);
+    if (!gameover) {
+      let result = playRound(playerSel, getComputerChoice());
+      let para1 = document.createElement('p');
+      para1.innerText = result;
+      info.innerHTML = result;
+      checkGame(playerScore, computerScore);
+    }
   });
 });
